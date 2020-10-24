9a6ccc4c4f14dbb1f3afbb7ea3d47a76c3618dc8
AbhijitBAutomation <abhijit.bhosale15@gmail.com>
2020-10-24 23:01:37 +0530

    msg

---
+++ src/balaji/abst/JustTesting.java
@@ -0,0 +1,10 @@
+package balaji.abst;
+
+public class TestClass {
+	public static void main(String[] args) {
+		MainClass obj=new MainClass();
+		obj.getVal("SampleCodez");
+		obj.display();
+	}
+
+}
